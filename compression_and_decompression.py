fa4419fedcb58ed4638c2ce0aac117b7c1c30013
Jairo Honorio <registrofx@gmail.com>
2019-07-15 00:38:33 -0300

    Solve first test case of compression_and_decompression challenge

--- compression_and_decompression.py
+++ compression_and_decompression.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 
+from enum import Enum
+
+character_type_enum = Enum("CHARACTER_TYPE_ENUM", "number letter open_bracket close_bracket")
 
 def decompress_string(input_string: str) -> str:
     """
@@ -40,5 +43,52 @@ def decompress_string(input_string: str) -> str:
     :return: the generated uncompressed string
     """
 
-    return input_string
+    output = ""
+    current_number_string = ""
+    decompression_depth = 0
+    current_string_to_decompress = ""
+
+    for current_character in input_string:
+
+        current_character_type = identify_character(current_character)
+
+        # Execute action depending on character type
+        if current_character_type == character_type_enum.letter:
+            if decompression_depth == 0:
+                output += current_character
+            else:
+                current_string_to_decompress += current_character
+
+        elif current_character_type == character_type_enum.number:
+            current_number_string = current_character
+
+        elif current_character_type == character_type_enum.open_bracket:
+            decompression_depth += 1
+
+        elif current_character_type == character_type_enum.close_bracket:
+            for _ in range(int(current_number_string)):
+                output += current_string_to_decompress
+            current_string_to_decompress = ""
+            decompression_depth -= 1
+
+    return output
+
+
+def identify_character(current_character: str) -> character_type_enum:
+    """
+    Checks whether the character is one of the four allowed types:
+    [, ], number or letter
+
+    :param current_character: the character to evaluate
+    :return: the type of the character as an character_type_enum
+    """
+    if current_character == '[':
+        current_character_type = character_type_enum.open_bracket
+    elif current_character == ']':
+        current_character_type = character_type_enum.close_bracket
+    elif current_character.isdigit():
+        current_character_type = character_type_enum.number
+    else:
+        current_character_type = character_type_enum.letter
+    return current_character_type
 
--- test_compression_and_decompression.py
+++ test_compression_and_decompression.py
@@ -10,3 +10,15 @@ class DecompressTest(TestCase):
         result = decompress_string(test_input)
         self.assertEqual(expected_output, result)
 
+    def test_number_with_more_than_one_digit(self):
+        test_input = "10[a]"
+        expected_output = "aaaaaaaaaa"
+        result = decompress_string(test_input)
+        self.assertEqual(expected_output, result)
+
+    def test_repetition_inside_another(self):
+        test_input = "2[3[a]b]"
+        expected_output = "aaabaaab"
+        result = decompress_string(test_input)
+        self.assertEqual(expected_output, result)
+
